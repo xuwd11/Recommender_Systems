abd6b3dc79831a98907afc0104d6dfb2bf5d5409
Weidong <xuwd11@gmail.com>
2017-11-27 18:07:08 -0500

    added IO

---
+++ recommender/IO.py
@@ -0,0 +1,15 @@
+import pickle
+
+class IO:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        
+    def to_pickle(self, obj):
+        with open(self.file_name, 'wb') as output:
+            pickle.dump(obj, output)
+        return self
+    
+    def read_pickle(self):
+        with open(self.file_name, 'rb') as input_:
+            obj = pickle.load(input_)
+        return obj
--- recommender/__init__.py
+++ recommender/__init__.py
@@ -1,3 +1,4 @@
 from .plot import plot_cm
+from .IO import IO
 from .Baseline import BaselineMean, BaselineRegression
 from .ALS import ALS1, ALS2
