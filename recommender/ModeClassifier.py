95a4f49c963388c85553bf1797de48d080613ad4
Weidong <xuwd11@gmail.com>
2017-11-27 21:17:49 -0500

    added ModeClassifier

---
+++ recommender/ModeClassifier.py
@@ -0,0 +1,30 @@
+import numpy as np
+import time
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
+
+class ModeClassifier:
+    def __init__(self, mode=5, classification=None):
+        self.mode = mode
+        self.fitted = True
+        self.classification = classification
+    
+    def predict(self, X, classification=None):
+        return self.mode * np.ones(len(X))
+    
+    def score(self, X, y, classification=None, scoring='r2'):
+        if classification is None:
+            classification = self.classification
+        if not classification:
+            if scoring == 'r2':
+                return r2_score(y, self.predict(X, classification))
+            elif scoring == 'mse':
+                return mean_squared_error(y, self.predict(X, classification))
+            elif scoring == 'rmse':
+                return np.sqrt(mean_squared_error(y, self.predict(X, classification)))
+            else:
+                raise NotImplementedError('`scoring` should be either "r2", "mse", or "rmse".')
+        else:
+            return accuracy_score(y, self.predict(X, classification))
--- recommender/__init__.py
+++ recommender/__init__.py
@@ -1,4 +1,5 @@
 from .plot import plot_cm
 from .IO import IO
+from .ModeClassifier import ModeClassifier
 from .Baseline import BaselineMean, BaselineRegression
 from .ALS import ALS1, ALS2
