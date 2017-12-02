d9809035b711655a2dba9f691ab44ac87c2b3aae
Weidong <xuwd11@gmail.com>
2017-12-01 19:01:34 -0500

    cleaned up

--- recommender/__init__.py
+++
@@ -1,6 +0,0 @@
-from .plot import plot_cm, print_results
-from .IO import IO
-from .ModeClassifier import ModeClassifier
-from .Baseline import BaselineMean, BaselineRegression
-from .ALS import ALS1, ALS2
-from .RS_surprise import RS_surprise
